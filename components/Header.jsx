0fbc8290dd555331ed151fb3a7616ad653f47df6
youssef yasser <youssifyasserme1999@gmail.com>
2023-01-18 23:56:26 +0200

    header center

--- components/Header.jsx
+++ components/Header.jsx
@@ -36,6 +36,18 @@ const Header = () => {
         </Link>
 
         {/* center */}
+        <div className="max-w-xs">
+          <div className="relative py-1 px-2 rounded-md">
+            <div className="absolute inset-0 pl-4 flex items-center pointer-events-none">
+              <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              type="text"
+              className="bg-gray-50 pl-8 sm:pl-10 block w-full sm:text-sm rounded-md border-gray-300 focus:ring-black focus:border-black"
+              placeholder="Search"
+            />
+          </div>
+        </div>
 
         {/* right */}
       </div>
