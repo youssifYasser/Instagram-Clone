75280a5f58836a1f8a3ba054aad2ee23cf687084
youssef yasser <youssifyasserme1999@gmail.com>
2023-01-21 18:15:28 +0200

    search bar refactoring width

--- components/Header.jsx
+++ components/Header.jsx
@@ -38,14 +38,14 @@ const Header = () => {
         </Link>
 
         {/* center */}
-        <div className="max-w-xs">
+        <div className="w-56 md:w-64 lg:w-72">
           <div className="relative py-1 px-2 rounded-md">
             <div className="absolute inset-0 pl-4 flex items-center pointer-events-none">
               <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
             </div>
             <input
               type="text"
-              className="bg-gray-50 pl-8 sm:pl-10 block w-full sm:text-sm rounded-md border-gray-300 focus:ring-black focus:border-black"
+              className="bg-gray-50 pl-8 sm:pl-10 block w-full text-sm md:text-base rounded-md border-gray-300 focus:ring-black focus:border-black"
               placeholder="Search"
             />
           </div>
