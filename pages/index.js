6ad6b8194adcc419ec933fcccb4107e6981aa176
youssef yasser <youssifyasserme1999@gmail.com>
2023-01-23 23:49:45 +0200

    modal added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Head from 'next/head';
+import CreatePostModal from '../components/CreatePostModal';
 import Feed from '../components/Feed';
 
 export default function Home() {
   return (
     <main className="bg-gray-50 h-screen">
+      <CreatePostModal />
       <Feed />
     </main>
   );
